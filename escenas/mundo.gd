51a7361da78827b151ee5e814f184f05b0ea0300
Tatomon1 <tatoraton3@outlook.com>
2019-06-03 17:11:57 -0300

    Agregué el scoreTimer
    Pude poner el score timer pero aun no se ve, y cuando perdes se reinicia el ScoreTimer, o sea, comienza de 0

--- escenas/mundo.gd
+++ escenas/mundo.gd
@@ -1,5 +1,5 @@
 extends Node
-var Score
+var Score = 0
 signal iniciojuego
 # Declare member variables here. Examples:
 # var a = 2
@@ -7,12 +7,20 @@ signal iniciojuego
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$ScoreTimer.start()
+	print(Score)
 	pass # Replace with function body.
 
 func juego_nuevo():
-	print(Score)
+	#print(Score)
 	Score = 0
 	$ScoreTimer.start()
 	
 	
 	
+
+
+func _on_ScoreTimer_timeout():
+	Score += 1
+	print(Score)
+	pass # Replace with function body.
