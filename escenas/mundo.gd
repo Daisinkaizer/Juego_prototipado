1369e1724cd84b00f3eeefdca2df40070d1a3daf
Tatomon1 <tatoraton3@outlook.com>
2019-06-23 14:03:48 -0300

    lpm qe me pario

--- escenas/mundo.gd
+++ escenas/mundo.gd
@@ -3,10 +3,10 @@ var Score = 0
 signal iniciojuego
 func _ready():
 	#$jugador.hide()
-	$generador_P.hide()
-	$generador_P2.hide()
-	$generador_T.hide()
-	$generador_T2.hide()
+#	$generador_P.hide()
+##	$generador_P2.hide()
+#	$generador_T.hide()
+#	$generador_T2.hide()
 	$InicioTimer.start()
 	
 	yield($InicioTimer, "timeout")
@@ -22,10 +22,10 @@ func juego_nuevo():
 	print(Score)
 	$jugador.show()
 	$jugador.inicio($Spawn.position)
-	$generador_P.show()
-	$generador_P2.show()
-	$generador_T.show()
-	$generador_T2.show()
+#	$generador_P.show()
+#	$generador_P2.show()
+#	$generador_T.show()
+#	$generador_T2.show()
 	
 
 
